70874c583cc07533c7c262aca75dee3d6dc710f9
Gongkirby <9040__@naver.com>
2024-11-29 21:54:42 +0900

    FIX : signup 기능 구현

--- src/main/java/controller/DispatcherServlet.java
+++ src/main/java/controller/DispatcherServlet.java
@@ -1,12 +1,14 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.dao.JDBCUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +16,9 @@ import org.slf4j.LoggerFactory;
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
-    
+
     private RequestMapping rm;
+    private JDBCUtil jdbcUtil;
 
     @Override
     public void init() throws ServletException {
@@ -24,36 +27,40 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) 
+    protected void service(HttpServletRequest request, HttpServletResponse response)
     	throws ServletException, IOException {
-    	logger.debug("Method : {}, Request URI : {}, ServletPath : {}", 
+    	logger.debug("Method : {}, Request URI : {}, ServletPath : {}",
     			request.getMethod(), request.getRequestURI(), request.getServletPath());
     	String contextPath = request.getContextPath();
     	String servletPath = request.getServletPath();
-    	
+
     	// URL 중 servletPath에 대응되는 controller를 구함
         Controller controller = rm.findController(servletPath);
         try {
         	// controller를 통해 request 처리 후, 이동할 uri를 반환 받음
             String uri = controller.execute(request, response);
-            
+
             if (uri == null) return;	// Ajax request 처리 완료
-            
- 			// 반환된 uri에 따라 forwarding 또는 redirection 여부를 결정하고 이동 
-            if (uri.startsWith("redirect:")) {	
+
+ 			// 반환된 uri에 따라 forwarding 또는 redirection 여부를 결정하고 이동
+            if (uri.startsWith("redirect:")) {
             	// redirection 지시
             	String targetUri = contextPath + uri.substring("redirect:".length());
-            	response.sendRedirect(targetUri);	// redirect to url            
+            	response.sendRedirect(targetUri);	// redirect to url
             }
             else {
             	// forwarding 수행
             	String targetUri = "/WEB-INF" + uri;
             	RequestDispatcher rd = request.getRequestDispatcher(targetUri);
                 rd.forward(request, response);		// forward to the view page
-            }                   
+            }
+        } catch (SQLException e) {
+            logger.error("SQLException occurred during user registration: ", e);
+            jdbcUtil.rollback();
+            throw new ServletException("Database error during user registration", e);
         } catch (Exception e) {
-            logger.error("Exception : {}", e);
-            throw new ServletException(e.getMessage());
+            throw new RuntimeException(e);
         }
+
     }
 }

901c5a4f27fcbe85995e7cabbbd690808a65d611
minju <iaki111@naver.com>
2024-12-19 01:41:59 +0900

    [fix] ottservice 오류 해결

--- src/main/java/util/Main.java
+++ src/main/java/util/Main.java
@@ -11,7 +11,7 @@ public class Main {
         // Selenium 크롤링 메서드 실행
     	// 3개 한꺼번에 돌리면 안돌아감
         //contentController.crawlAndSaveMovies();
-        //contentController.crawlAndSaveDramas();
-        contentController.crawlAndSaveAnimations();
+        contentController.crawlAndSaveDramas();
+        //contentController.crawlAndSaveAnimations();
     }
 }
